6f333286164e505bfea266d0c455ea2065d1a298
Hanzalah.Mohammed <hanzalah.mohammed@theoptimum.net>
2021-04-29 12:50:33 +0800

    user pojo added

--- src/pojo/User.java
+++ src/pojo/User.java
@@ -1,4 +1,41 @@
 package pojo;
 
 public class User {
+
+    private String userEmail;
+    private String userPassword;
+    private String securityKey;
+    private double bankBalance;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getSecurityKey() {
+        return securityKey;
+    }
+
+    public void setSecurityKey(String securityKey) {
+        this.securityKey = securityKey;
+    }
+
+    public double getBankBalance() {
+        return bankBalance;
+    }
+
+    public void setBankBalance(double bankBalance) {
+        this.bankBalance = bankBalance;
+    }
 }
