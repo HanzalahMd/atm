45237af8017aee738e5407f508f418ec0b13c98d
Hanzalah.Mohammed <hanzalah.mohammed@theoptimum.net>
2021-04-29 14:13:10 +0800

    userRegistration() completed with validation and exception handling

--- src/service/RegisterImplementation.java
+++ src/service/RegisterImplementation.java
@@ -9,35 +9,60 @@ public class RegisterImplementation implements RegisterInterface {
 
     Scanner refScanner = new Scanner(System.in);
     DataAccessInterface refAccess;
-    boolean validated;
 
     @Override
     public void UserRegistration() {
 
+        String userInputEmail = null;
+        String userInputPassword = null;
+        String userInputKey;
+        boolean emailValidated = false;
+        boolean passwordEntered = false;
+        boolean passwordValidated = false;
+        boolean registrationComplete = false;
+
         User existingUser = new User();
         existingUser.insertData("xyz@gmail.com", "xyz", "xyz");
 
-        while(!validated) {
+        while (!registrationComplete){
             try {
-                System.out.println("Enter email address: ");
-                String userInputEmail = refScanner.next();
+                while(!emailValidated) {
+                    System.out.println("Enter email address: ");
+                    userInputEmail = refScanner.next();
 
-                if (userInputEmail.equals(existingUser.getUserEmail())) {
-                    throw new Exception("email already exists!!" + "\n");
-                }
+                    if (userInputEmail.equals(existingUser.getUserEmail())) {
+                        throw new Exception("email already exists!!" + "\n");
+                    }
+                    emailValidated = true;
+                } // End of while (!emailValidated) loop
 
-                System.out.println("Enter Password: ");
-                String userInputPassword = refScanner.next();
+                while (!passwordValidated) {
+                    if(!passwordEntered) {
+                        System.out.println("Enter Password: ");
+                        userInputPassword = refScanner.next();
+                        passwordEntered = true;
+                    } else {
+                        System.out.println("Retype Password: ");
+                        String confirmPassword = refScanner.next();
+
+                        if (!confirmPassword.equals(userInputPassword)) {
+                            throw new Exception("Password doesn't match!!");
+                        }
+                        passwordValidated = true;
+                    }
+                } // End of while (!passwordValidated) loop
 
                 System.out.println("What is favourite colour ?");
-                String userInputKey = refScanner.next();
+                userInputKey = refScanner.next();
+                System.out.println(userInputKey + " is your security key, in case if you forget your password.");
 
                 refAccess = new DataAccessImplementation();
                 refAccess.createNewUser(userInputEmail, userInputPassword, userInputKey);
-                validated = true;
+                registrationComplete = true;
+
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
-        }
-    }
+        } // End of while (!registrationComplete) loop
+    } // End of userRegistration() method
 }
