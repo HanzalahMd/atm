ed8508bfcd001b7e60faa5207aff230f865383ea
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-01 06:58:36 +0800

    comments added

--- src/pojo/User.java
+++ src/pojo/User.java
@@ -2,17 +2,20 @@ package pojo;
 
 public class User {
 
+    // generate necessary variables and make it private
     private String userEmail;
     private String userPassword;
     private String securityKey;
     private double bankBalance;
 
+    // use setter method to create one new user and pass all the values input using parameters
     public void insertData(String userEmail, String userPassword, String securityKey){
         this.userEmail = userEmail;
         this.userPassword = userPassword;
         this.securityKey = securityKey;
     }
 
+    // Getter methods - to get the user values
     public String getUserEmail() {
         return userEmail;
     }
@@ -26,6 +29,7 @@ public class User {
         return bankBalance;
     }
 
+    // Setter methods - to modify user values
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
     }
