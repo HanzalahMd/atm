32b27860fb76e8e89695dec62876a6222857fda9
Hanzalah.Mohammed <hanzalah.mohammed@theoptimum.net>
2021-04-30 09:02:38 +0800

    Main class completed

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,22 +12,21 @@ public class Main {
         boolean sessionActive = true;
 
         while(sessionActive) {
-            System.out.println();
-            System.out.println("User Home Page: \n 1) Register \n 2) Login \n 3) Forget Password \n 4) Logout \n");
+            System.out.println("\n User Home Page: \n 1) Register \n 2) Login \n 3) Forget Password \n 4) Logout \n");
             System.out.println("Enter you choice: ");
 
-        int userChoice = refScanner.nextInt();
+            int userChoice = refScanner.nextInt();
 
-        if (userChoice == 1) {
-            refController.callRegisterService();
-        } else if (userChoice == 2){
-            refController.callLoginService();
-        } else if (userChoice == 3){
-            refController.callResetService();
-        } else if (userChoice == 4) {
-            sessionActive = false;
-            System.out.println("Logout Successful");
-        }
+            if (userChoice == 1) {
+                refController.callRegisterService();
+            } else if (userChoice == 2){
+                refController.callLoginService();
+            } else if (userChoice == 3){
+                refController.callResetService();
+            } else if (userChoice == 4) {
+                sessionActive = false;
+                System.out.println("Logout Successful");
+            }
         }
     }
 }
