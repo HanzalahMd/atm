1b900da981e558bd477677cbc336d6e7566e91d1
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-01 06:43:27 +0800

    add exception handling for inputs other than 1-4

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,22 +12,26 @@ public class Main {
         boolean sessionActive = true; // always be true until the user logout.
 
         while(sessionActive) { // will always run this loop while sessionActive is true
-            System.out.println("\n User Home Page: \n 1) Register \n 2) Login \n 3) Forget Password \n 4) Logout \n");
-            System.out.println("Enter you choice: ");
+            try {
+                System.out.println("\n User Home Page: \n 1) Register \n 2) Login \n 3) Forget Password \n 4) Logout \n");
+                System.out.println("Enter you choice: ");
 
-            int userChoice = refScanner.nextInt();
+                int userChoice = refScanner.nextInt();
 
-            if (userChoice == 1) {
-                refController.callRegisterService(); // call register service in controller
-            } else if (userChoice == 2){
-                refController.callLoginService(); // call login service in controller
-            } else if (userChoice == 3){
-                refController.callResetService(); // call reset service in controller
-            } else if (userChoice == 4) {
-                sessionActive = false; // to stop from running this while loop
-                System.out.println("Logout Successful");
-            } else { // for inputs other than 1-4
-                System.out.println("Invalid input");
+                if (userChoice == 1) {
+                    refController.callRegisterService(); // call register service in controller
+                } else if (userChoice == 2) {
+                    refController.callLoginService(); // call login service in controller
+                } else if (userChoice == 3) {
+                    refController.callResetService(); // call reset service in controller
+                } else if (userChoice == 4) {
+                    sessionActive = false; // to stop from running this while loop
+                    System.out.println("Logout Successful");
+                } else { // for inputs other than 1-4
+                    throw new Exception("Invalid input!!");
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
             }
         }
     }
