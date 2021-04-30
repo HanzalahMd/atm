cd3e81200128740c77b5a7ef4fc1120c58d7b71c
HanzalahMd <hanzalahmohammed@gmail.com>
2021-05-01 07:10:02 +0800

    comments added

--- src/dao/DataAccessImplementation.java
+++ src/dao/DataAccessImplementation.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class DataAccessImplementation implements DataAccessInterface{
 
-    User refUser;
+    User refUser; // initialize the reference for User object globally so the other methods can access the same user.
     Scanner refScanner = new Scanner(System.in);
 
     @Override
@@ -19,12 +19,12 @@ public class DataAccessImplementation implements DataAccessInterface{
     @Override
     public boolean authenticateUser(String userEmail, String userPassword) {
         return userEmail.equals(refUser.getUserEmail()) && userPassword.equals(refUser.getUserPassword());
-    }
+    } // this method will validate user inputs and the User email and password
 
     @Override
     public boolean checkSecurityKey(String userEmail, String securityKey) {
         return userEmail.equals(refUser.getUserEmail()) && securityKey.equals(refUser.getSecurityKey());
-    }
+    } // this method will validate user inputs with User email and security key
 
     @Override
     public void checkBalance(){
@@ -36,9 +36,9 @@ public class DataAccessImplementation implements DataAccessInterface{
         System.out.println("Enter Amount to deposit:");
         double amount = refScanner.nextDouble();
 
-        if(amount < 0){
+        if(amount < 0){ // reject depositing negative amount
             System.out.println("Amount can't be negative!!");
-        } else {
+        } else { // accepts if its positive amount
             refUser.setBankBalance(refUser.getBankBalance() + amount);
             System.out.println("$" + amount + " deposited successfully!");
             System.out.println("Your new balance is $" + refUser.getBankBalance());
@@ -50,9 +50,9 @@ public class DataAccessImplementation implements DataAccessInterface{
         System.out.println("Enter Amount to withdraw:");
         double amount = refScanner.nextDouble();
 
-        if(amount > refUser.getBankBalance()){
+        if(amount > refUser.getBankBalance()){ // reject withdrawing amount higher than bank balance
             System.out.println("Sorry!! insufficient balance.");
-        } else {
+        } else { // accepts if its same or lower than bank balance
             refUser.setBankBalance(refUser.getBankBalance() - amount);
             System.out.println("Transaction successful!");
             System.out.println("Your new balance is $" + refUser.getBankBalance());
